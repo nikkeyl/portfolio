d55170b46f8a0342a09c326d14dee9c3d60d0178
nikkeyl <131481562+nikkeyl@users.noreply.github.com>
2023-07-24 10:53:33 +0300

    refactor: Remove extra code

--- src/js/modules/addLoadedClass.js
+++ src/js/modules/addLoadedClass.js
@@ -1,8 +1,20 @@
 import { html } from '@js/helpers/nodeList'
 
 function addLoadedClass() {
-	if (!html.classList.contains('loading')) {
-		window.addEventListener('load', html.classList.add('loaded'))
+	window.addEventListener('load', html.classList.add('loaded'))
+
+	const preloader = document.querySelector('.preloader')
+	const preloaderContainer = document.querySelector('#preloader')
+
+	if (html.classList.contains('loaded')) {
+		setInterval(() => {
+			preloader.classList.add('hide')
+			preloaderContainer.classList.add('hide')
+			setTimeout(() => {
+				preloader.remove()
+				preloaderContainer.remove()
+			}, 300)
+		}, 300)
 	}
 }
 
