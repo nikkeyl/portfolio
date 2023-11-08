346cfd80bde37064cc85ec252aafc08e1b0068ad
nikkeyl <131481562+nikkeyl@users.noreply.github.com>
2023-11-08 19:37:45 +0300

    update

--- src/js/vendors/Typed.js
+++ src/js/vendors/Typed.js
@@ -1,18 +1,11 @@
 import Typed from 'typed.js';
 
-import { html } from '@js/helpers/nodeList';
-
 const typedText = () => {
 	const typedElement = document.querySelector('[data-typed]');
-	const string = html.classList.contains('halloween')
-		? 'Magic'
-		: html.classList.contains('new-year')
-		? 'Miracle'
-		: 'Development';
 
 	if (typedElement) {
 		new Typed(typedElement, {
-			strings: [string],
+			strings: ['Development'],
 			typeSpeed: 50,
 			loop: false,
 			fadeOut: true
